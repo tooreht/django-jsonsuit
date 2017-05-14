0e0463aa640a9048fbbe17cf842bebc2e7341e14
tooreht <tooreht@gmail.com>
2017-05-14 21:58:11 +0200

    Fix minor package management issues

--- setup.py
+++ setup.py
@@ -10,18 +10,8 @@ except ImportError:
     from distutils.core import setup
 
 
-def get_version(*file_paths):
-    """Retrieves the version from jsonsuit/__init__.py"""
-    filename = os.path.join(os.path.dirname(__file__), *file_paths)
-    version_file = open(filename).read()
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError('Unable to find version string.')
-
-
-version = get_version("jsonsuit", "__init__.py")
+# Managed by bumpversion
+version = '0.1.0'
 
 def convert_md_to_rst(filename):
     try:
@@ -76,6 +66,7 @@ setup(
         'Framework :: Django :: 1.8',
         'Framework :: Django :: 1.9',
         'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
@@ -85,5 +76,6 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 )
