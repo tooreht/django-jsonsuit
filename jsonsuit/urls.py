0ed50ca09d7df82a29cd756eecee1138fb6622ff
tooreht <tooreht@gmail.com>
2017-06-02 14:36:42 +0200

    Set the app_name attribute in the included module

--- jsonsuit/urls.py
+++ jsonsuit/urls.py
@@ -2,6 +2,7 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
 
+app_name = 'jsonsuit'
 urlpatterns = [
     url(r'', TemplateView.as_view(template_name="base.html")),
 ]
--- tests/urls.py
+++ tests/urls.py
@@ -3,8 +3,6 @@ from __future__ import unicode_literals, absolute_import
 
 from django.conf.urls import url, include
 
-from jsonsuit.urls import urlpatterns as jsonsuit_urls
-
 urlpatterns = [
-    url(r'^', include(jsonsuit_urls, namespace='jsonsuit')),
+    url(r'^', include('jsonsuit.urls', namespace='jsonsuit')),
 ]
