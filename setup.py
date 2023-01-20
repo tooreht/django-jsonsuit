cdb4731296b093256a73bb0b53f95023b4f8cac0
tooreht <tooreht@gmail.com>
2023-01-20 23:04:28 +0100

    Simplify setup

--- setup.py
+++ setup.py
@@ -24,15 +24,6 @@ def get_version(*file_paths):
 version = get_version("jsonsuit", "__init__.py")
 
 
-def convert_md_to_rst(filename):
-    try:
-       import pypandoc
-       rst = pypandoc.convert_file('{}.md'.format(filename), 'rst')
-    except (IOError, ImportError):
-       rst = ''
-    return rst
-
-
 if sys.argv[-1] == 'publish':
     try:
         import wheel
@@ -50,36 +41,14 @@ if sys.argv[-1] == 'tag':
     os.system("git push --tags")
     sys.exit()
 
-readme = convert_md_to_rst('README')
-history = convert_md_to_rst('HISTORY')
 requirements = open('requirements.txt').readlines()
 
 setup(
-    name='django-jsonsuit',
     version=version,
-    description="""Django goodies to dress JSON data in a suit.""",
-    long_description=readme + '\n\n' + history,
-    author='Marc Zimmermann',
-    author_email='tooreht@gmail.com',
-    url='https://github.com/tooreht/django-jsonsuit',
     packages=[
         'jsonsuit',
     ],
     include_package_data=True,
     install_requires=requirements,
-    python_requires='>=3.9',
-    license="MIT",
     zip_safe=False,
-    keywords=['django-jsonsuit', 'django', 'json', 'suit'],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-    ],
 )
